eaf90b1b234fd1537060bdd6d2da4d0d8ea84391
Chihori Suzuki <moonwetwithhoney@mail.com>
2020-12-14 18:20:52 -0800

    Initial Commit

---
+++ tourStrangeRooms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tourStrangeRooms
+//
+//  Created by 鈴木ちほり on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
